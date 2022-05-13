e9530c11fd34bdb9cf87ff59c20090542d3bd0af
scyth3-c <kevintor2246@gmail.com>
2022-05-12 19:51:01 -0500

    some minimal changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <http.h>
-
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-	HTTP tasty("http://example.com/");
+	
 
-    tasty.get();
-    
-	cout<<tasty.genPerfomList();
 
 	return 0;
 }
---
+++ src/room.cpp
@@ -0,0 +1,8 @@
+#include "room.h"
+
+Room::Room() {
+
+    
+
+
+}
