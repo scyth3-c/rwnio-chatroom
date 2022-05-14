173cc76bd68158be0ba1db0d321e5cf787dc20f4
scyth3-c <kevintor2246@gmail.com>
2022-05-14 16:28:41 -0500

    update http, added custom POST method

--- src/http.cpp
+++ src/http.cpp
@@ -12,6 +12,8 @@ HTTP::~HTTP() {
 	curl.reset();
 }
 
+
+// callback function, this extract response of querys
 size_t HTTP::callback(void* buffer, size_t size, size_t nmemb, string* data) {
 	size_t rsize = size * nmemb;
 	try {
@@ -24,6 +26,7 @@ size_t HTTP::callback(void* buffer, size_t size, size_t nmemb, string* data) {
 }
 
 
+//base http get
 void HTTP::get()
 {
 	curl_easy_setopt(*curl, CURLOPT_URL, URL.c_str());
@@ -40,6 +43,10 @@ void HTTP::get()
 	curl_easy_cleanup(*curl);
 }
 
+
+
+
+// base http post
 void HTTP::post(string fields) {
 
 	curl_easy_setopt(*curl, CURLOPT_URL, URL.c_str());
@@ -58,10 +65,39 @@ void HTTP::post(string fields) {
 	curl_easy_cleanup(*curl);
 }
 
+
+
+
+// post with endpoints
+void HTTP::post(string endpoint, string fields) {
+
+    string url = URL + endpoint;
+	curl_easy_setopt(*curl, CURLOPT_URL,  url.c_str());
+
+	curl_easy_setopt(*curl, CURLOPT_WRITEFUNCTION, HTTP::callback);
+	curl_easy_setopt(*curl, CURLOPT_POSTFIELDS, fields.c_str());
+	curl_easy_setopt(*curl, CURLOPT_WRITEDATA, &response);
+	
+	CURLcode perfom = curl_easy_perform(*curl);
+	performs.push_back(perfom);
+
+	try {
+		if(perfom != CURLE_OK) screen(std::clog, RWNIO_WARNING,CURL_DECORATOR,perfom);
+	} catch(std::exception& error) {
+		screen(std::cout, RWNIO_ERROR, &error);
+	}
+	curl_easy_cleanup(*curl);
+}
+
+
+
+//extract response of the querys
 string HTTP::Response() {
 	return without(response, char(34));
 }
 
+
+//remove separator send of the API
 string HTTP::without(string target, char key ) {
 	for (auto &it : target) {
 		if (it == key) { it = char(00); }
@@ -69,6 +105,8 @@ string HTTP::without(string target, char key ) {
 	return target;
 }
 
+
+// extract the list of result of querys
 string HTTP::genPerfomList() {
 	int map{};
 	for(auto &it: performs) {
