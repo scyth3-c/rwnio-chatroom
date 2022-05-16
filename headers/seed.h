d4a19516fdfdab3de6ee70ca792e6fb82bad9b54
scyth3-c <kevintor2246@gmail.com>
2022-05-15 21:58:31 -0500

    update seed

--- headers/seed.h
+++ headers/seed.h
@@ -10,26 +10,24 @@
 
 using std::string;
 
-class Seed {
+class Seed
+{
 
 private:
- 
     unsigned char digest[SHA_DIGEST_LENGTH];
-    const char* cadena{};
+    const char *cadena{};
     std::unique_ptr<string> SEED = nullptr;
 
 public:
-
     Seed(string);
     Seed();
     ~Seed();
     void genSeed();
-    void setPlain(const char*);
+    void setPlain(const char *);
     string getSeed();
     string getSeedX();
     string genRandInit();
     string SHA(string);
 };
-
 
 #endif // !_SEED_H
