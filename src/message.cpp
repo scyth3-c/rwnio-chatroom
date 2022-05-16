afa18a508405aac70e10de336203c5c918422ca9
scyth3-c <kevintor2246@gmail.com>
2022-05-15 21:56:53 -0500

    update message

--- src/message.cpp
+++ src/message.cpp
@@ -1,10 +1,12 @@
 #include "message.h"
 
-Message::Message(string _creador,string _secreto)  {
+Message::Message(string _creador, string _secreto)
+{
 	creador = make_shared<string>(_creador);
 	secreto = make_shared<string>(_secreto);
 }
-Message::~Message() {
+Message::~Message()
+{
 	http.reset();
 	URL.reset();
 	creador.reset();
@@ -12,26 +14,27 @@ Message::~Message() {
 	secreto.reset();
 }
 
- void Message::setUrl(string temp) {	URL = make_shared<string>(temp); }
+void Message::setUrl(string temp) { URL = make_shared<string>(temp); }
 
-string Message::send(string mensaje) {
-	http = make_shared<HTTP>(*URL+"insert");
+string Message::send(string mensaje)
+{
+	http = make_shared<HTTP>(*URL + "insert");
 	http->post(compact(mensaje));
 	string response = http->Response();
 	http.reset();
 	return response;
 }
 
-MSG_t Message::last() {
-	
+MSG_t Message::last(int _sort)
+{
 	http = make_shared<HTTP>(*URL);
-	http->post("last", "secreto="+*secreto);
+	http->post("last", "secreto=" + *secreto + "&sort=" + std::to_string(_sort));
 	auto response = reasing(http->Response());
 	http.reset();
 	return response;
 }
 
-string Message::compact(string _mensaje) { return "creador=" + (*creador) + "&contenido=" + _mensaje + "&secreto="+*secreto;}
+string Message::compact(string _mensaje) { return "creador=" + (*creador) + "&contenido=" + _mensaje + "&secreto=" + *secreto; }
 
 MSG_t Message::reasing(string target)
 {
@@ -40,30 +43,39 @@ MSG_t Message::reasing(string target)
 	string contenido{};
 	string secreto{};
 
-	string* it = &contenido;
+	string *it = &contenido;
 	int pos = 0;
 	for (int i = 0; i < target.length(); i++)
 	{
-		if (target[i] == ';' && pos==0) {
+		if (target[i] == ';' && pos == 0)
+		{
 			pos = 1;
 			it = &creador;
 		}
-		else if(target[i] == ';' && pos == 1) {
+		else if (target[i] == ';' && pos == 1)
+		{
 			it = &hora;
 			pos = 2;
-		} else if(target[i] == ';' && pos == 2) {
-			it =& secreto;
+		}
+		else if (target[i] == ';' && pos == 2)
+		{
+			it = &secreto;
 		}
 		*it += target[i];
 	}
-	string temp [4] = {clearify(hora),clearify(creador),clearify(contenido), clearify(secreto)};
+	string temp[4] = {clearify(hora), clearify(creador), clearify(contenido), clearify(secreto)};
 	MSG_t tasty(temp);
 	return tasty;
 }
 
-string Message::clearify(string target) {
-		for (auto& it : target) {
-			if (it == ';') { it = char(00); }
+string Message::clearify(string target)
+{
+	for (auto &it : target)
+	{
+		if (it == ';')
+		{
+			it = char(00);
 		}
-		return target;
+	}
+	return target;
 }
