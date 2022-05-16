503602152ddbd36363036b95d05d58fd3051bc4b
scyth3-c <kevintor2246@gmail.com>
2022-05-15 21:59:50 -0500

    update custom

--- headers/custom.h
+++ headers/custom.h
@@ -1,48 +1,65 @@
 #ifndef CUSTOM_H
 #define CUSTOM_H
 
-
 #include <string>
 #include <memory>
 #include <vector>
 #include <thread>
 #include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
 #include "http.h"
 #include "screen.h"
 #include "room_modules.h"
 #include "rwnio.h"
 #include "message.h"
 
-using std::string;
-using std::shared_ptr;
+// static define members
+#define _MSG std::cin >> Custom::_msg;
+#define _MSG_t Custom::_msg
+#define _MSG_UP string Custom::_msg = "";
+
+#define CLOCK Custom::_clock
+#define CLOCK_UP unsigned int Custom::_clock = 4;
+// end
+
 using std::make_shared;
+using std::shared_ptr;
+using std::string;
 using std::vector;
 
-class Custom {
+using std::thread;
 
-    private:
+class Custom
+{
 
-    shared_ptr<Message> message_t = nullptr; 
+private:
+    shared_ptr<Message> message_view = nullptr;
+    shared_ptr<Message> message_send = nullptr;
+
     shared_ptr<Screen> screen = nullptr;
     shared_ptr<Modules> modules = nullptr;
 
     shared_ptr<string> hash = nullptr;
     shared_ptr<string> name = nullptr;
 
-    vector<MSG_t*> messages;
+    vector<MSG_t *> messages;
 
-    public:
+    static string _msg;
+    static unsigned int _clock;
 
+public:
     Custom(string, string);
     ~Custom();
-    
+
     void loopCustom();
     void charge();
-    static void view(Custom*);
 
-
+    static void viewLoop(Custom *);
+    void sendLoop();
+    void send(string msg);
+    void model(string, string, string);
 };
-
-
 
 #endif // ! CUSTOM_H
